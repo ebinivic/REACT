78f58fe36f67ee4d872dfdba1b9b49402b1daea0
Ebini Adetola Victor <77898095+ebinivic@users.noreply.github.com>
2024-08-14 12:18:38 +0000

    eight commit

--- Reactwiz/src/components/Fruits.jsx
+++ Reactwiz/src/components/Fruits.jsx
@@ -1,8 +1,14 @@
 export default function Fruits() {
-    const Fruits = ["Apple", "Mango", "Banana", "Orange", "Pineapples"];
+    //const Fruits = ["Apple", "Mango", "Banana", "Orange", "Pineapples"];
+    const Fruits =[ 
+        {name:"Apple", price:10, emoji:''},
+        {name:"Peas", price:13, emoji:''},
+        {name:"Oranges", price:14, emoji:''},
+    
+]
     return <div> 
         <ul>
-            {Fruits.map((Fruit)=>(<li key={Fruit}>{Fruit}</li>))}
+            {Fruits.map((Fruit)=>(<li key={Fruit}>{''}{Fruit.emoji} {Fruit.name} ${Fruit.price}</li>))}
              </ul>
     </div>;
 }
